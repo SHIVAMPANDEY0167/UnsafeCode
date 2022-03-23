571127495b791bf17a5050f92a10a4110194fdf5
Shivam Pandey <shivam.pandey@1Rintern.local>
2022-03-23 14:32:51 +0530

    Unsafe code Push

---
+++ unsafe/unsafe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace unsafe1
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+            
+            
+            int num1;
+            unsafe
+            {
+                Console.WriteLine("Enter the number to check odd or even");
+                num1 = int.Parse(Console.ReadLine());
+                int* ptr;
+                ptr = &num1;
+                if (*ptr % 2 == 0)
+                {
+                    Console.WriteLine("Number is Even");
+                }
+                else
+                {
+                    Console.WriteLine("Number is odd");
+                }
+                Console.ReadKey();
+            }
+            
+
+     }
+}
+}
